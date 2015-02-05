mndrix/State-Machine

commit 395174973406f31f6d9ee1f3badb08af765a217d

Optimized next-transition deducing functionality.

lib/State/Machine.pm
@@ -26,14 +26,7 @@ has 'topic' => (
 
 method apply {
     my $state = $self->state;
-    my $next  = shift // $state->next;
-
-    if ($state && !$next) {
-        # deduce transition unless defined
-        if ($state->transitions->keys->count == 1) {
-            $next = $state->transitions->keys->get(0);
-        }
-    }
+    my $next  = $self->next;
 
     # cannot transition
     State::Machine::Failure::Transition::Missing->throw
@@ -48,6 +41,7 @@ method apply {
         catch {
             # transition execution failure
             State::Machine::Failure::Transition::Execution->throw(
+                captured          => $_,
                 transition_name   => $next,
                 transition_object => $trans,
             );
@@ -64,7 +58,17 @@ method apply {
 };
 
 method next {
-    return $self->state->next;
+    my $state = $self->state;
+    my $next  = shift // $state->next;
+
+    if ($state && !$next) {
+        # deduce transition unless defined
+        if ($state->transitions->keys->count == 1) {
+            $next = $state->transitions->keys->get(0);
+        }
+    }
+
+    return $next;
 }
 
 method status {

lib/State/Machine/Failure/Transition/Execution.pm
@@ -4,9 +4,17 @@ package State::Machine::Failure::Transition::Execution;
 use Bubblegum::Class;
 use Function::Parameters;
 
+use Bubblegum::Constraints -typesof;
+
 extends 'State::Machine::Failure::Transition';
 
 # VERSION
+
+has 'captured' => (
+    is       => 'ro',
+    isa      => typeof_defined,
+    required => 1
+);
 
 method _build_message {
     "Transition execution failure."

commit 99e8291bca94c28bc8c6014aea10dd4dcafac767

Fix calling apply() with an argument
Commit 395174973406f31f6d9ee1f3badb08af765a217d broke the “apply”
method when invoked with a single argument.  I added a failing test and
fixed the mistake.

lib/State/Machine.pm
@@ -26,7 +26,7 @@ has 'topic' => (
 
 method apply {
     my $state = $self->state;
-    my $next  = $self->next;
+    my $next  = $self->next(shift);
 
     # cannot transition
     State::Machine::Failure::Transition::Missing->throw

t/state/machine.t
@@ -68,4 +68,40 @@ subtest 'light-switch circular-state example' => sub {
     is $d, 1, '$turn_off -during- hook executed';
 };
 
+subtest 'horse race' => sub {
+    my $m = 'State::Machine';
+    my $s = 'State::Machine::State';
+    my $t = 'State::Machine::Transition';
+
+    # state
+    my $standing = $s->new(name => 'standing');
+    my $running  = $s->new(name => 'running');
+
+    # transitions
+    my $quiet = $t->new(name => 'quiet', result => $standing);
+    my $bell  = $t->new(name => 'bell', result => $running);
+
+    # bind
+    $standing->add_transition($quiet);
+    $standing->add_transition($bell);
+
+    # build machine
+    my $race = $m->new(
+        topic => 'horse race',
+        state => $standing # initial state
+    );
+
+    # standing -> standing
+    $race->apply('quiet');
+    is $race->state->name, 'standing', 'horse standing behind the gate';
+
+    # standing -> standing, again
+    $race->apply('quiet');
+    is $race->state->name, 'standing', 'horse still standing';
+
+    # standing -> running
+    $race->apply('bell');
+    is $race->state->name, 'running', 'horse running down the track';
+};
+
 ok 1 and done_testing;